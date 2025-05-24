b3b74fc8ab396f6ddbba12aae6fe415afeca8019
Hero24-00 <196810131+Hero24-x@users.noreply.github.com>
2025-05-24 08:06:55 +0530

    Create alert.py

---
+++ alert.py
@@ -0,0 +1,7 @@
+def send_alert(message):
+    """
+    Send alert to user.
+    For now, just print to console.
+    Can be extended to Telegram, Discord, email, etc.
+    """
+    print(f"[ALERT] {message}")
